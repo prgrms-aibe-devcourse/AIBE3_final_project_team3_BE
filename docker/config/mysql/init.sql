558f3fd72017bd77399a5b34054dcb69b6e444c1
leere1 <dlfhrdms@gmail.com>
2025-11-07 16:25:26 +0900

    문장미니게임 엔티티 추가, 번역 태그 엔티티 추가

--- docker/config/mysql/init.sql
+++ docker/config/mysql/init.sql
@@ -1,3 +1,11 @@
 CREATE USER IF NOT EXISTS 'mixchat_user'@'%' IDENTIFIED BY 'qwer1234';
 GRANT ALL PRIVILEGES ON `mysql_db`.* TO 'mixchat_user'@'%';
 FLUSH PRIVILEGES;
+
+USE mysql_db;
+
+INSERT INTO translation_tags (code) VALUES
+('GRAMMAR'),
+('SPELLING'),
+('STRUCTURE'),
+('WORD_UNKNOWN')
