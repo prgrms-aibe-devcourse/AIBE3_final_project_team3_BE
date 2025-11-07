aeff6303f8b9780119377555563120cee7992619
inti0 <sonin223@gmail.com>
2025-11-07 15:36:27 +0900

    feat : accessToken기반 필터체인 구현

--- src/main/java/triplestar/mixchat/global/security/JwtAuthorizationFilter.java
+++ src/main/java/triplestar/mixchat/global/security/JwtAuthorizationFilter.java
@@ -1,53 +1,59 @@
 package triplestar.mixchat.global.security;
 
-import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import triplestar.mixchat.global.jwt.AccessTokenPayload;
 import triplestar.mixchat.global.jwt.AuthJwtProvider;
 
-// 다음 PR 에서 마저 작성될 코드
+// AccessToken 기반 인증 처리 필터
 @Component
 @RequiredArgsConstructor
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final String BEARER_PREFIX = "Bearer ";
-    private static final String REFRESH_TOKEN = "RefreshToken";
+
     private final AuthJwtProvider authJwtProvider;
+    private final CustomUserDetailsService customUserDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
         String header = request.getHeader(AUTHORIZATION_HEADER);
+
         if (header != null && header.startsWith(BEARER_PREFIX)) {
             String token = header.substring(BEARER_PREFIX.length());
             boolean isValid = authJwtProvider.validateAccessToken(token);
             if (isValid) {
-                filterChain.doFilter(request, response);
-                return;
+                AuthenticateByToken(token);
+            } else {
+                throw new BadCredentialsException("유효하지 않은 액세스 토큰입니다.");
             }
         }
 
-        String refreshToken = findRefreshTokenOnCookie(request);
-
+        filterChain.doFilter(request, response);
     }
 
-    private String findRefreshTokenOnCookie(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        return Arrays.stream(cookies).filter
-                        (cookie -> cookie.getName().equals(REFRESH_TOKEN))
-                .findFirst()
-                .orElseThrow(() -> new AccessDeniedException("Refresh token is missing"))
-                .getValue();
+    private void AuthenticateByToken(String token) {
+        AccessTokenPayload accessTokenPayload = authJwtProvider.parseAccessToken(token);
+        UserDetails userDetails = customUserDetailsService
+                .loadUserByUsername(accessTokenPayload.memberId().toString());
+
+        Authentication authToken =
+                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authToken);
     }
 }
