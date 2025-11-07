03b1019b236649dd9179dc10552be2b8d4a7c3f9
leere1 <dlfhrdms@gmail.com>
2025-11-07 16:25:26 +0900

    문장미니게임 엔티티 추가, 번역 태그 엔티티 추가

--- src/main/java/triplestar/mixchat/domain/translation/translation/entity/TranslationTag.java
+++ src/main/java/triplestar/mixchat/domain/translation/translation/entity/TranslationTag.java
@@ -1,24 +1,25 @@
-package triplestar.mixchat.domain.miniGame.sentenceGame.entity;
+package triplestar.mixchat.domain.translation.translation.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
+import lombok.NoArgsConstructor;
 import triplestar.mixchat.domain.translation.translation.constant.TranslationTagCode;
 import triplestar.mixchat.global.jpa.entity.BaseEntity;
 
+@NoArgsConstructor
 @Entity
-@Table(name = "sentenceGames")
-public class SentenceGame extends BaseEntity {
-
-    @Column(nullable = false)
-    private String originalContent;      // 수정 전 문장
-
-    @Column(nullable = false)
-    private String correctedContent;     // 수정 후 문장
+@Table(name = "translation_tags")
+public class TranslationTag extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private TranslationTagCode code;
+
+
+    public TranslationTag(TranslationTagCode code) {
+        this.code = code;
+    }
 }
