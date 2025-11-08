6f0f4d2769cc3295f24de1b72159fc5337210812
inti0 <sonin223@gmail.com>
2025-11-09 02:06:49 +0900

    feat : 친구요청 entity 생성

---
+++ src/main/java/triplestar/mixchat/domain/member/friend/entity/FriendshipRequest.java
@@ -0,0 +1,27 @@
+package triplestar.mixchat.domain.member.friend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import triplestar.mixchat.domain.member.member.entity.Member;
+import triplestar.mixchat.global.jpa.entity.BaseEntity;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "friendship_requests")
+public class FriendshipRequest extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member receiver;
+
+    @Enumerated(EnumType.STRING)
+    private FriendshipRequestStatus status;
+}
