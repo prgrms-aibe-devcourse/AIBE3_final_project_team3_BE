d80008c8fbb8250e8e29a54958f499ddf05bcfb1
inti0 <sonin223@gmail.com>
2025-11-09 02:23:24 +0900

    feat : 친구요청, 친구 repository 생성

---
+++ src/main/java/triplestar/mixchat/domain/member/friend/repository/FriendshipRepository.java
@@ -0,0 +1,9 @@
+package triplestar.mixchat.domain.member.friend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import triplestar.mixchat.domain.member.friend.entity.Friendship;
+
+@Repository
+public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
+}
