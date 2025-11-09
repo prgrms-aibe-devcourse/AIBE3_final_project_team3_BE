d86b1776c9887385edcc052b11f7171e9402ff24
inti0 <sonin223@gmail.com>
2025-11-09 17:18:31 +0900

    feat : 친구 컨트롤러 추가

--- src/main/java/triplestar/mixchat/domain/member/friend/entity/Friendship.java
+++ src/main/java/triplestar/mixchat/domain/member/friend/entity/Friendship.java
@@ -1,6 +1,5 @@
 package triplestar.mixchat.domain.member.friend.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
