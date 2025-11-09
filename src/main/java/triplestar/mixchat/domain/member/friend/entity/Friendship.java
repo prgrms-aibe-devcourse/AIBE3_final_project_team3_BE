fbce5d45f041013651e0d647bfb178e21ad43dee
inti0 <sonin223@gmail.com>
2025-11-10 00:28:18 +0900

    refactor : BaseEntityNoModified 추가

--- src/main/java/triplestar/mixchat/domain/member/friend/entity/Friendship.java
+++ src/main/java/triplestar/mixchat/domain/member/friend/entity/Friendship.java
@@ -2,26 +2,18 @@ package triplestar.mixchat.domain.member.friend.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
 import triplestar.mixchat.domain.member.member.entity.Member;
+import triplestar.mixchat.global.jpa.entity.BaseEntityNoModified;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "friendships")
-public class Friendship {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+public class Friendship extends BaseEntityNoModified {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "smaller_member_id", nullable = false)
@@ -30,9 +22,6 @@ public class Friendship {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "larger_member_id", nullable = false)
     private Member largerMember;
-
-    @CreatedDate
-    private LocalDateTime createdAt;
 
     public Friendship(Member smallerMember, Member largerMember) {
         this.smallerMember = smallerMember;
--- src/main/java/triplestar/mixchat/domain/member/friend/entity/FriendshipRequest.java
+++ src/main/java/triplestar/mixchat/domain/member/friend/entity/FriendshipRequest.java
@@ -2,28 +2,20 @@ package triplestar.mixchat.domain.member.friend.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
 import triplestar.mixchat.domain.member.member.entity.Member;
+import triplestar.mixchat.global.jpa.entity.BaseEntityNoModified;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "friendship_requests")
 @Getter
-public class FriendshipRequest {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class FriendshipRequest extends BaseEntityNoModified {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "sender_id", nullable = false)
@@ -32,9 +24,6 @@ public class FriendshipRequest {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "receiver_id", nullable = false)
     private Member receiver;
-
-    @CreatedDate
-    private LocalDateTime createdAt;
 
     public FriendshipRequest(Member sender, Member receiver) {
         this.sender = sender;
