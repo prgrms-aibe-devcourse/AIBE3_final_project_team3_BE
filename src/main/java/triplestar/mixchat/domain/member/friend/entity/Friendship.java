c8ba80c2136c2006da2c40f7f9081edf9fb18f8e
inti0 <sonin223@gmail.com>
2025-11-09 13:11:46 +0900

    feat : 친구관련 서비스 생성

--- src/main/java/triplestar/mixchat/domain/member/friend/entity/Friendship.java
+++ src/main/java/triplestar/mixchat/domain/member/friend/entity/Friendship.java
@@ -2,6 +2,7 @@ package triplestar.mixchat.domain.member.friend.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -22,14 +23,19 @@ public class Friendship {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @Column(name = "smaller_id")
     private Member smallerMember;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @Column(name = "larger_id")
     private Member largerMember;
 
     @CreatedDate
     private LocalDateTime createdAt;
+
+    public Friendship(Member smallerMember, Member largerMember) {
+        this.smallerMember = smallerMember;
+        this.largerMember = largerMember;
+    }
 }
---
+++ src/main/java/triplestar/mixchat/domain/member/friend/service/FriendshipRequestService.java
@@ -0,0 +1,87 @@
+package triplestar.mixchat.domain.member.friend.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import triplestar.mixchat.domain.member.friend.entity.FriendshipRequest;
+import triplestar.mixchat.domain.member.friend.repository.FriendshipRequestRepository;
+import triplestar.mixchat.domain.member.member.entity.Member;
+import triplestar.mixchat.domain.member.member.repository.MemberRepository;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class FriendshipRequestService {
+
+    private final FriendshipService friendshipService;
+    private final FriendshipRequestRepository friendshipRequestRepository;
+    private final MemberRepository memberRepository;
+
+    /**
+     * 친구 요청 생성
+     */
+    public void sendRequest(Long senderId, Long receiverId) {
+        Member sender = findMemberById(senderId);
+        Member receiver = findMemberById(receiverId);
+        validateFriendshipRequest(sender, receiver);
+
+        FriendshipRequest friendshipRequest = new FriendshipRequest(sender, receiver);
+        friendshipRequestRepository.save(friendshipRequest);
+    }
+
+    private void validateFriendshipRequest(Member sender, Member receiver) {
+        if (sender.equals(receiver)) {
+            throw new IllegalArgumentException("자기 자신에게 친구 요청을 보낼 수 없습니다");
+        }
+        boolean friendshipExists = friendshipService.isFriends(sender.getId(), receiver.getId());
+        if (friendshipExists) {
+            throw new IllegalStateException("이미 친구 관계입니다");
+        }
+
+        boolean requestExists = friendshipRequestRepository
+                .existsBySenderAndReceiver(sender, receiver);
+        boolean reverseRequestExists = friendshipRequestRepository
+                .existsBySenderAndReceiver(receiver, sender);
+        if (requestExists || reverseRequestExists) {
+            throw new IllegalStateException("이미 친구 요청이 존재합니다");
+        }
+    }
+
+    private Member findMemberById(Long id) {
+        return memberRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 회원입니다"));
+    }
+
+    /**
+     * 친구 요청 처리
+     */
+    public void processRequest(Long memberId, Long requestId, boolean isAccepted) {
+        FriendshipRequest request = validateFriendshipRequest(memberId, requestId);
+
+        if (isAccepted) {
+            friendshipService.createFriendship(request.getSender(), request.getReceiver());
+            request.accept();
+        } else {
+            request.reject();
+        }
+
+        // NOTE : 친구 요청 softDelete? hardDelete? 현재는 softDelete로 구현
+        // 이 경우 어떻게 쌓인 데이터를 관리할지 고민 필요
+        friendshipRequestRepository.save(request);
+    }
+
+    private FriendshipRequest validateFriendshipRequest(Long memberId, Long requestId) {
+        FriendshipRequest request = friendshipRequestRepository.findByIdWithSenderReceiver(requestId)
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 친구 요청입니다"));
+
+        if (!request.getReceiver().getId().equals(memberId)) {
+            throw new IllegalArgumentException("본인의 친구 요청만 처리할 수 있습니다");
+        }
+
+        if (!request.isPending()) {
+            throw new IllegalStateException("이미 처리된 친구 요청입니다");
+        }
+        return request;
+    }
+}
