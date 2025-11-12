cf97bdb2a618de5b7da2f6504d0c9abbeb46f070
inti0 <sonin223@gmail.com>
2025-11-12 15:57:40 +0900

    feat : void 성공 메시지 공통화

--- src/main/java/triplestar/mixchat/global/springdoc/SuccessResponse.java
+++ src/main/java/triplestar/mixchat/global/springdoc/SuccessResponse.java
@@ -1,6 +1,7 @@
 package triplestar.mixchat.global.springdoc;
 
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import java.lang.annotation.ElementType;
@@ -10,13 +11,21 @@ import java.lang.annotation.Target;
 
 /**
  * 모든 API 엔드포인트의 성공 응답(200 OK) 스키마를 정의합니다.
+ * ApiResponse<Void>인 경우 일반적인 성공 메시지를 자동으로 포함시킵니다.
+ * 응답 반환값이 있는 경우 해당 타입에 맞는 스키마가 자동으로 적용됩니다.
  */
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @ApiResponse(
         responseCode = "200",
         description = "요청 처리 성공",
-        content = @Content(schema = @Schema(implementation = ApiResponse.class))
+        content = @Content(
+                schema = @Schema(implementation = ApiResponse.class),
+                examples = @ExampleObject(
+                        summary = "일반적인 성공 메시지",
+                        value = "{\"msg\": \"요청 성공 메시지(메시지 내용은 다를 수 있음)\"}"
+                )
+        )
 )
 public @interface SuccessResponse {
 }
