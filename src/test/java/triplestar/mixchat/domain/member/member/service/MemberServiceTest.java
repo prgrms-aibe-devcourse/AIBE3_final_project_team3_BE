02d5e46215d9c5bd1f7cc5ff77de1b606304ca5c
inti0 <sonin223@gmail.com>
2025-11-14 03:19:11 +0900

    feat : 회원 상세정보 조회 서비스/리포지토리 메소드

--- src/test/java/triplestar/mixchat/domain/member/member/service/MemberServiceTest.java
+++ src/test/java/triplestar/mixchat/domain/member/member/service/MemberServiceTest.java
@@ -1,7 +1,9 @@
 package triplestar.mixchat.domain.member.member.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,8 +13,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+import triplestar.mixchat.domain.member.friend.service.FriendshipRequestService;
 import triplestar.mixchat.domain.member.member.constant.EnglishLevel;
 import triplestar.mixchat.domain.member.member.dto.MemberInfoModifyReq;
+import triplestar.mixchat.domain.member.member.dto.MemberProfileResp;
 import triplestar.mixchat.domain.member.member.entity.Member;
 import triplestar.mixchat.domain.member.member.repository.MemberRepository;
 import triplestar.mixchat.testutils.TestMemberFactory;
@@ -28,6 +32,9 @@ class MemberServiceTest {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    FriendshipRequestService friendshipRequestService;
 
     Member member;
 
@@ -89,5 +96,81 @@ class MemberServiceTest {
         // minio에 UUID로 저장되므로 파일 이름이 아닌 확장자만 확인
         assertThat(updatedMember.getProfileImageUrl()).isNotEqualTo("http://localhost:9000/test-bucket/default-profile.webp");
         assertThat(updatedMember.getProfileImageUrl()).endsWith(".png");
+    }
+
+    @Test
+    @DisplayName("회원 상세 조회 - 비회원 조회 성공")
+    void get_member_details_non_member_success() {
+        MemberProfileResp resp = memberService.getMemberDetails(null, member.getId());
+
+        assertThat(resp.memberId()).isEqualTo(member.getId());
+        assertThat(resp.email()).isEqualTo(member.getEmail());
+        assertThat(resp.name()).isEqualTo(member.getName());
+        assertThat(resp.nickname()).isEqualTo(member.getNickname());
+        assertThat(resp.country()).isEqualTo(member.getCountry());
+        assertThat(resp.englishMastery()).isEqualTo(member.getEnglishLevel());
+        assertThat(resp.interests()).isEqualTo(member.getInterests());
+        assertThat(resp.description()).isEqualTo(member.getDescription());
+        assertThat(resp.profileImageUrl()).isEqualTo(member.getProfileImageUrl());
+        assertThat(resp.isFriend()).isFalse();
+        assertThat(resp.isPendingRequest()).isFalse();
+    }
+
+    @Test
+    @DisplayName("회원 상세 조회 - 회원 조회 성공(친구 아님, 친구 신청 대기중 아님)")
+    void get_member_details_member_no_friend_no_request_success() {
+        Member member2 = memberRepository.save(TestMemberFactory.createMember("test2"));
+
+        MemberProfileResp resp = memberService.getMemberDetails(member.getId(), member2.getId());
+
+        assertThat(resp.isFriend()).isFalse();
+        assertThat(resp.isPendingRequest()).isFalse();
+    }
+
+    @Test
+    @DisplayName("회원 상세 조회 - 회원 조회 성공(친구 아님, 친구 신청 대기중)")
+    void get_member_details_member_success() {
+        Member member2 = memberRepository.save(TestMemberFactory.createMember("test2"));
+
+        // member가 member2에게 친구 신청
+        friendshipRequestService.sendRequest(member.getId(), member2.getId());
+
+        MemberProfileResp resp = memberService.getMemberDetails(member.getId(), member2.getId());
+
+        assertThat(resp.isFriend()).isFalse();
+        assertThat(resp.isPendingRequest()).isTrue();
+    }
+
+    @Test
+    @DisplayName("회원 상세 조회 - 회원 조회 성공(친구)")
+    void get_member_details_member_friend_success() {
+        Member member2 = memberRepository.save(TestMemberFactory.createMember("test2"));
+
+        // member가 member2에게 친구 신청
+        Long requestId = friendshipRequestService.sendRequest(member.getId(), member2.getId());
+        // member2가 친구 신청 수락
+        friendshipRequestService.processRequest(member2.getId(), requestId, true);
+
+        MemberProfileResp resp = memberService.getMemberDetails(member.getId(), member2.getId());
+
+        assertThat(resp.isFriend()).isTrue();
+        assertThat(resp.isPendingRequest()).isFalse();
+    }
+
+    @Test
+    @DisplayName("회원 상세 조회 - 회원 조회 실패(존재하지 않는 회원)")
+    void get_member_details_member_not_found_fail() {
+        assertThatThrownBy(() -> memberService.getMemberDetails(member.getId(), Long.MAX_VALUE))
+                .isInstanceOf(EntityNotFoundException.class)
+                .hasMessageContaining("존재하지 않는 회원입니다.");
+    }
+
+    @Test
+    @DisplayName("회원 상세 조회 - 자기 자신 조회 성공")
+    void get_member_details_self_success() {
+        MemberProfileResp resp = memberService.getMemberDetails(member.getId(), member.getId());
+
+        assertThat(resp.isFriend()).isFalse();
+        assertThat(resp.isPendingRequest()).isFalse();
     }
 }
