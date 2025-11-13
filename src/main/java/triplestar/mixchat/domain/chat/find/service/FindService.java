dec6cf3fdf61b3aee1a3e6a7dcba71a969316d14
Chan <rlackswhd200@gmail.com>
2025-11-13 15:04:43 +0900

    feat: find 도메인 작업중

---
+++ src/main/java/triplestar/mixchat/domain/chat/find/service/FindService.java
@@ -0,0 +1,24 @@
+package triplestar.mixchat.domain.chat.find.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import triplestar.mixchat.domain.member.member.dto.MemberSummaryResp;
+import triplestar.mixchat.domain.member.member.repository.MemberRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class FindService {
+
+    private final MemberRepository memberRepository;
+
+    public List<MemberSummaryResp> findAllMembers() {
+        return memberRepository.findAll().stream()
+                .map(MemberSummaryResp::new)
+                .collect(Collectors.toList());
+    }
+}
