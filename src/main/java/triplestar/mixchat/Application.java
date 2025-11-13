115b5d944e5d2ea3e9caab8a607f783014a6d84d
Chan <rlackswhd200@gmail.com>
2025-11-13 18:51:13 +0900

    feat: find 엔티티 작성
    # Conflicts:
    #	src/main/resources/application-secret-default.yml
    
    # Conflicts:
    #	src/main/java/triplestar/mixchat/global/security/JwtAuthorizationFilter.java

--- src/main/java/triplestar/mixchat/Application.java
+++ src/main/java/triplestar/mixchat/Application.java
@@ -3,9 +3,15 @@ package triplestar.mixchat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableMongoAuditing
+@EnableJpaRepositories(basePackages = "triplestar.mixchat.domain") // JPA 리포지토리 경로
+@EnableMongoRepositories(basePackages = "triplestar.mixchat.domain.chat.chat.repository") // MongoDB 리포지토리 경로
 public class Application {
 
     public static void main(String[] args) {
