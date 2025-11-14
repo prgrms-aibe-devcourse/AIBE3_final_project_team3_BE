0058897e03a1e2377c2b27645c0c5d87d48f3c6e
inti0 <sonin223@gmail.com>
2025-11-15 04:12:54 +0900

    refactor : 알람 서비스, 리포지토리 수정

--- src/main/java/triplestar/mixchat/domain/notification/repository/NotificationRepository.java
+++ src/main/java/triplestar/mixchat/domain/notification/repository/NotificationRepository.java
@@ -1,8 +1,10 @@
 package triplestar.mixchat.domain.notification.repository;
 
+import java.time.LocalDateTime;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import triplestar.mixchat.domain.member.member.entity.Member;
@@ -10,11 +12,31 @@ import triplestar.mixchat.domain.notification.entity.Notification;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    Page<Notification> findAllByReceiverId(Long receiverId, Pageable pageable);
+
     @Query("""
-                select n from Notification n
-                join fetch n.receiver.id
-                where n.receiver = :receiver
+                update Notification n
+                set n.isRead = true
+                where n.receiver.id = :receiverId
             """
     )
-    Page<Notification> findAllByReceiver(Member receiver, Pageable pageable);
+    @Modifying
+    void markAllAsRead(Long receiverId);
+
+    @Query("""
+                delete from Notification n
+                where n.receiver.id = :receiverId
+            """
+    )
+    @Modifying
+    void deleteAllByReceiver(Long receiverId);
+
+    @Query("""
+                delete from Notification n
+                where n.createdAt < :threshold
+            """
+    )
+    @Modifying
+    void deleteOld(LocalDateTime threshold);
 }
--- src/main/java/triplestar/mixchat/domain/notification/service/NotificationService.java
+++ src/main/java/triplestar/mixchat/domain/notification/service/NotificationService.java
@@ -1,9 +1,13 @@
 package triplestar.mixchat.domain.notification.service;
 
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import triplestar.mixchat.domain.member.member.entity.Member;
@@ -21,18 +25,24 @@ public class NotificationService {
     private final MemberRepository memberRepository;
     private final NotificationRepository notificationRepository;
 
-    private Member findMemberById(Long memberId) {
-        return memberRepository.findById(memberId)
-                .orElseThrow(() -> new EntityNotFoundException("해당 회원 ID 없음: " + memberId));
-    }
-
-    private Notification findNotificationById(Long id) {
-        return notificationRepository.findById(id)
+    private Notification findNotificationById(Long memberId, Long id) {
+        Notification notification = notificationRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("해당 알람 ID 없음: " + id));
+
+        // id를 조회할 때는 N+1 문제가 발생하지 않음
+        if (!notification.getReceiver().getId().equals(memberId)) {
+            throw new AccessDeniedException("해당 회원의 알람이 아님: " + id);
+        }
+
+        return notification;
     }
 
+    /**
+     * 알림 생성
+     */
     public NotificationResp createNotification(NotificationReq req) {
-        Member receiver = findMemberById(req.receiverId());
+        Member receiver = memberRepository.findById(req.receiverId())
+                .orElseThrow(() -> new EntityNotFoundException("해당 회원 ID 없음"));
 
         Notification notification = new Notification(
                 receiver,
@@ -51,10 +61,11 @@ public class NotificationService {
         );
     }
 
-    public Page<NotificationResp> getNotifications(Long memberId, Pageable pageable) {
-        Member receiver = findMemberById(memberId);
-
-        Page<Notification> notifications = notificationRepository.findAllByReceiver(receiver, pageable);
+    /**
+     * 알림 조회(페이징)
+     */
+    public Page<NotificationResp> getNotifications(Long receiverId, Pageable pageable) {
+        Page<Notification> notifications = notificationRepository.findAllByReceiverId(receiverId, pageable);
 
         return notifications.map(notification -> new NotificationResp(
                 notification.getId(),
@@ -65,32 +76,38 @@ public class NotificationService {
         ));
     }
 
-    public void markAsRead(Long id) {
-        findNotificationById(id).read();
+    /**
+     * 읽음 처리
+     */
+    public void markAsRead(Long memberId, Long id) {
+        findNotificationById(memberId, id).read();
     }
 
     public void markAllAsRead(Long memberId) {
-        Member receiver = findMemberById(memberId);
-        Page<Notification> notifications = notificationRepository.findAllByReceiver(receiver, Pageable.unpaged());
-        notifications.forEach(Notification::read);
+        notificationRepository.markAllAsRead(memberId);
     }
 
-    public void deleteNotification(Long id) {
-        notificationRepository.deleteById(id);
+    /**
+     * 삭제
+     */
+    public void deleteNotification(Long memberId, Long id) {
+        Notification notification = findNotificationById(memberId, id);
+        notificationRepository.delete(notification);
     }
 
     public void deleteAllNotifications(Long memberId) {
-        Member receiver = findMemberById(memberId);
-        Page<Notification> notifications = notificationRepository.findAllByReceiver(receiver, Pageable.unpaged());
-        notificationRepository.deleteAll(notifications.getContent());
+        notificationRepository.deleteAllByReceiver(memberId);
     }
 
-    // TODO: 읽은 알람만 삭제
-    public void deleteReadNotifications(Long memberId) {
+    // TODO: 기준일수 주입받기
+    private void deleteOldNotifications() {
+        int daysThreshold = 30;
+        LocalDateTime threshold = LocalDateTime.now().minusDays(daysThreshold);
+        notificationRepository.deleteOld(threshold);
     }
 
-    // TODO: 일정 기간 지난 알람 삭제, 기준일수 주입받기
-    public void deleteOldNotifications() {
-
+    @Scheduled(cron = "0 0 3 * * ?") // 매일 새벽 3시에 실행
+    public void scheduledDeleteOldNotifications() {
+        deleteOldNotifications();
     }
 }
