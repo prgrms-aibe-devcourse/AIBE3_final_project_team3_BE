8945172e2990fe34cafcf113364aa1bc6d75c09c
Chan <rlackswhd200@gmail.com>
2025-11-16 21:32:52 +0900

    find 컨트롤러 삭제

--- src/main/java/triplestar/mixchat/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/triplestar/mixchat/domain/member/member/controller/ApiV1MemberController.java
@@ -1,17 +1,23 @@
 package triplestar.mixchat.domain.member.member.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import triplestar.mixchat.domain.chat.find.service.FindService;
+import triplestar.mixchat.domain.member.auth.dto.MemberSummaryResp;
 import triplestar.mixchat.domain.member.member.dto.MemberInfoModifyReq;
 import triplestar.mixchat.domain.member.member.service.MemberService;
 import triplestar.mixchat.global.response.CustomResponse;
 import triplestar.mixchat.global.security.CustomUserDetails;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/members")
@@ -19,6 +25,14 @@ import triplestar.mixchat.global.security.CustomUserDetails;
 public class ApiV1MemberController implements ApiMemberController {
 
     private final MemberService memberService;
+    private final FindService findService;
+
+    @Operation(summary = "모든 회원 목록 조회", description = "채팅 상대로 추가할 수 있는 모든 회원 목록을 조회합니다. 자기 자신은 목록에서 제외됩니다.")
+    @GetMapping
+    public CustomResponse<List<MemberSummaryResp>> findAllMembers(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        List<MemberSummaryResp> members = findService.findAllMembers(userDetails.getId());
+        return CustomResponse.ok("모든 회원 목록을 성공적으로 조회했습니다.", members);
+    }
 
     @Override
     @PutMapping("/profile")
