c1bace9cd712e936e810047daf27dcdcc9840ed8
inti0 <sonin223@gmail.com>
2025-11-20 16:58:23 +0900

    style : 불필요한 import 제거

--- src/main/java/triplestar/mixchat/domain/chat/chat/repository/ChatMessageRepository.java
+++ src/main/java/triplestar/mixchat/domain/chat/chat/repository/ChatMessageRepository.java
@@ -2,7 +2,6 @@ package triplestar.mixchat.domain.chat.chat.repository;
 
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 import triplestar.mixchat.domain.chat.chat.entity.ChatMessage;
 
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
