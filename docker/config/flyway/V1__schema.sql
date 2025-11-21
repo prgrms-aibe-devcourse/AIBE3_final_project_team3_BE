f55ff8852ed0a8ff5cf665998d392be9be2158db
inti0 <sonin223@gmail.com>
2025-11-21 15:11:03 +0900

    feat : 학습노트, 신고 관련 스키마 정리

--- docker/config/flyway/V1__schema.sql
+++ docker/config/flyway/V1__schema.sql
@@ -5,6 +5,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
 CREATE SCHEMA IF NOT EXISTS `mysql_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `mysql_db` ;
 
+--  멤버 테이블
 CREATE TABLE IF NOT EXISTS `members` (
     `id`                 BIGINT        NOT NULL AUTO_INCREMENT,
     `created_at`         DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
@@ -38,7 +39,6 @@ CREATE TABLE IF NOT EXISTS `members` (
 
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-
 
 CREATE TABLE IF NOT EXISTS `friendships` (
                                              `id`                 BIGINT        NOT NULL AUTO_INCREMENT,
@@ -74,91 +74,83 @@ CREATE TABLE IF NOT EXISTS `friendship_requests` (
     FOREIGN KEY (`receiver_id`) REFERENCES `members` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
+-- AI 테이블
+CREATE TABLE IF NOT EXISTS `prompts` (
+    `id`            BIGINT        NOT NULL AUTO_INCREMENT,
+    `created_at`    DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    `modified_at`   DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
 
+    `member_id`     BIGINT        NULL,
 
-CREATE TABLE IF NOT EXISTS `reports` (
-                                         `id`              BIGINT       NOT NULL AUTO_INCREMENT,
-                                         `created_at`      DATETIME(6)  NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-                                         `modified_at`     DATETIME(6)  NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-                                         `reported_msg_content`  TEXT         NULL,
-                                         `target_member_id` BIGINT      NOT NULL,
-                                         `status`          VARCHAR(10)  NOT NULL,
-                                         `category`          VARCHAR(20) NOT NULL,
-                                         `reported_reason`     VARCHAR(255) NULL,
+    `prompt_type`   VARCHAR(20)   NOT NULL,
+    `title`         VARCHAR(255)  NOT NULL,
+    `content`       TEXT          NOT NULL,
+    `scenario_id`   VARCHAR(50)   NOT NULL UNIQUE,
 
-                                         PRIMARY KEY (`id`),
-                                         KEY `idx_reports_status_created` (`status`, `created_at`)
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`member_id`) REFERENCES `members` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `sentence_games` (
-                                                `id`             BIGINT       NOT NULL AUTO_INCREMENT,
-                                                `created_at`     DATETIME(6)  NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-
-                                                `original_content`  TEXT         NOT NULL,
-                                                `corrected_content` TEXT         NOT NULL,
-                                                `code`            VARCHAR(50)  NOT NULL,
-
-                                                PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-CREATE TABLE IF NOT EXISTS `translation_tags` (
-                                                  `id`       BIGINT       NOT NULL AUTO_INCREMENT,
-                                                  `code`     VARCHAR(50)  NOT NULL UNIQUE,
-                                                  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-# 프롬프트 테이블
-CREATE TABLE IF NOT EXISTS `userPrompts` (
-                                         `id`            BIGINT       NOT NULL AUTO_INCREMENT,
-                                         `created_at`    DATETIME(6)  NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-                                         `modified_at`   DATETIME(6)  NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-
-                                         `member_id`     BIGINT       NOT NULL,
-
-                                         `prompt_type`   VARCHAR(20)  NOT NULL,
-                                         `title`         VARCHAR(255) NOT NULL,
-                                         `content`       TEXT         NOT NULL,
-                                         `scenario_id`   VARCHAR(50)  NOT NULL UNIQUE,
-
-                                         PRIMARY KEY (`id`),
-                                         FOREIGN KEY (`member_id`) REFERENCES `members`(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
+-- 학습노트 테이블
 CREATE TABLE IF NOT EXISTS `learning_notes` (
-                                                `id`                BIGINT       NOT NULL AUTO_INCREMENT,
-                                                `member_id`         BIGINT       NOT NULL,
-                                                `original_content`  VARCHAR(100)         NOT NULL,
-                                                `corrected_content` VARCHAR(100)         NOT NULL,
-                                                `created_at`    DATETIME(6)  NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    `id`                BIGINT        NOT NULL AUTO_INCREMENT,
+    `member_id`         BIGINT        NOT NULL,
+    `original_content`  VARCHAR(100)  NOT NULL,
+    `corrected_content` VARCHAR(100)  NOT NULL,
+    `created_at`        DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
 
-                                                PRIMARY KEY (`id`),
-                                                KEY `idx_learning_notes_member_id` (`member_id`),
+    PRIMARY KEY (`id`),
+    KEY `idx_learning_notes_member_id` (`member_id`),
 
-                                                CONSTRAINT `fk_learning_note_member`
-                                                FOREIGN KEY (`member_id`)
-                                                REFERENCES `members` (`id`)
-                                                ON DELETE CASCADE
+    CONSTRAINT `fk_learning_note_member`
+    FOREIGN KEY (`member_id`) REFERENCES `members` (`id`)
+    ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `feedbacks` (
-                                           `id`               BIGINT       NOT NULL AUTO_INCREMENT,
-                                           `learning_note_id` BIGINT       NOT NULL,
-                                           `created_at`    DATETIME(6)  NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-                                           `modified_at`   DATETIME(6)  NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-                                           `tag`              VARCHAR(50)  NOT NULL,
-                                           `problem`          VARCHAR(100)         NOT NULL,
-                                           `correction`       VARCHAR(100)         NOT NULL,
-                                           `extra`            VARCHAR(100)         NOT NULL,
-                                           `is_marked`         BOOLEAN      NOT NULL DEFAULT FALSE,
+    `id`               BIGINT       NOT NULL AUTO_INCREMENT,
+    `learning_note_id` BIGINT       NOT NULL,
+    `created_at`       DATETIME(6)  NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    `modified_at`      DATETIME(6)  NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    `tag`              VARCHAR(50)  NOT NULL,
+    `problem`          VARCHAR(100) NOT NULL,
+    `correction`       VARCHAR(100) NOT NULL,
+    `extra`            VARCHAR(100) NOT NULL,
+    `is_marked`        BOOLEAN      NOT NULL DEFAULT FALSE,
 
+    PRIMARY KEY (`id`),
+    KEY `idx_learning_feedbacks_learning_note_id` (`learning_note_id`),
 
-                                           PRIMARY KEY (`id`),
-                                           KEY `idx_learning_feedbacks_learning_note_id` (`learning_note_id`),
+    CONSTRAINT `fk_feedback_note`
+    FOREIGN KEY (`learning_note_id`)
+    REFERENCES `learning_notes` (`id`)
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-                                           CONSTRAINT `fk_feedback_note`
-                                           FOREIGN KEY (`learning_note_id`)
-                                           REFERENCES `learning_notes` (`id`)
-                                           ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS `sentence_games` (
+    `id`                BIGINT        NOT NULL AUTO_INCREMENT,
+    `created_at`        DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+
+    `original_content`  TEXT          NOT NULL,
+    `corrected_content` TEXT          NOT NULL,
+
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 신고 테이블
+CREATE TABLE IF NOT EXISTS `reports` (
+    `id`                   BIGINT        NOT NULL AUTO_INCREMENT,
+    `created_at`           DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    `modified_at`          DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+
+    `reported_msg_content` TEXT          NULL,
+    `target_member_id`     BIGINT        NOT NULL,
+    `status`               VARCHAR(10)   NOT NULL,
+    `category`             VARCHAR(20)   NOT NULL,
+    `reported_reason`      VARCHAR(255)  NULL,
+
+    PRIMARY KEY (`id`),
+    KEY `idx_reports_status_created` (`status`, `created_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 SET SQL_MODE=@OLD_SQL_MODE;
