307aa0e6362790085c7f3b0d570e98d3db3a0e70
Leere1 <159905810+Leere1@users.noreply.github.com>
2025-12-12 15:40:37 +0900

    ♻️ Refactor(admin-post) : req 타입 변경 (#199)
    * refactor:req 타입 변경
    
    * Integer로 수정
    
    ---------
    
    Co-authored-by: inti0 <142914095+inti0@users.noreply.github.com>

--- src/main/java/triplestar/mixchat/domain/admin/admin/dto/AdminPostDeleteReq.java
+++ src/main/java/triplestar/mixchat/domain/admin/admin/dto/AdminPostDeleteReq.java
@@ -1,9 +1,11 @@
 package triplestar.mixchat.domain.admin.admin.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "관리자용 게시글 삭제 요청")
 public record AdminPostDeleteReq(
         @Schema(description = "삭제 사유", example = "2")
-        int reasonCode
+        @NotNull
+        Integer reasonCode
 ) {}
